4f0e6cb299e96a321a7ef4608fd82a313bab857b
ANUJ PANDEY <2018034@iiitdmj.ac.in>
2022-02-10 17:10:59 +0530

    Password removed

--- dj_celery/settings.py
+++ dj_celery/settings.py
@@ -146,5 +146,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = '3000timesfake@gmail.com'
-EMAIL_HOST_PASSWORD = 'vjwssonvafnlklnt'
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'Celery <3000timesfake@gmail.com>'
